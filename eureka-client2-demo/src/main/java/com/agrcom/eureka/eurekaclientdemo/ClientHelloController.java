99b613d59afd493959656428455ad47451369e8e
Daniel Dabrowski <agrcom@yahoo.pl>
2019-03-02 23:32:44 +0100

    added new controller for instance info

--- eureka-client2-demo/src/main/java/com/agrcom/eureka/eurekaclientdemo/ClientHelloController.java
+++ eureka-client2-demo/src/main/java/com/agrcom/eureka/eurekaclientdemo/ClientHelloController.java
@@ -3,11 +3,16 @@ package com.agrcom.eureka.eurekaclientdemo;
 import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ClientHelloController implements IGreetingController {
@@ -15,6 +20,9 @@ public class ClientHelloController implements IGreetingController {
     @Autowired
     @Lazy
     EurekaClient eurekaClient;
+
+    @Autowired
+    DiscoveryClient discoveryClient;
 
     @Value("${spring.application.name}")
     private String appName;
@@ -28,6 +36,12 @@ public class ClientHelloController implements IGreetingController {
     public ResponseEntity<String> hello(){
         return new ResponseEntity<>("hello", HttpStatus.OK);
     }
+
+    @GetMapping("/serviceInfo/{applicationName}")
+    public List<ServiceInstance> serviceInfo(@PathVariable String applicationName){
+        return this.discoveryClient.getInstances(applicationName);
+    }
+
 
 
 }
--- eureka-client2-demo/src/main/java/com/agrcom/eureka/eurekaclientdemo/EurekaClientDemoApplication.java
+++ eureka-client2-demo/src/main/java/com/agrcom/eureka/eurekaclientdemo/EurekaClientDemoApplication.java
@@ -2,8 +2,10 @@ package com.agrcom.eureka.eurekaclientdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaClientDemoApplication {
 
 	public static void main(String[] args) {
