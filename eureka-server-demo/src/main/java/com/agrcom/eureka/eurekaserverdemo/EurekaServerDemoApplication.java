2dc87d43305fc8eb8efea3af91e37368a0615158
Daniel Dabrowski <agrcom@yahoo.pl>
2019-03-02 12:05:53 +0100

    First commit

---
+++ eureka-server-demo/src/main/java/com/agrcom/eureka/eurekaserverdemo/EurekaServerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.agrcom.eureka.eurekaserverdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerDemoApplication.class, args);
+	}
+
+}
